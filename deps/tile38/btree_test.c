1b6454393d3d9ff8ac42f28ea1c4b5409eacd559
Josh Baker <joshbaker77@gmail.com>
2016-04-22 14:34:46 -0700

    rename proj to tile38

---
+++ deps/tile38/btree_test.c
@@ -0,0 +1,7 @@
+//#ifdef TILE38_TEST
+
+int test_BTree(){
+	return 1;
+}
+
+//#endif
---
+++ deps/tile38/tile38_test.c
@@ -0,0 +1,71 @@
+//#ifdef TILE38_TEST
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+
+int test_WKT();
+int test_BTree();
+
+typedef struct test{
+	char *name;
+	int (*test)();
+} test;
+
+test tests[] = {
+	{ "wkt", test_WKT },
+	{ "btree", test_BTree },
+};
+
+void sig_handler(int sig) {
+	printf("\x1b[0m\n");
+	exit(-1);
+}
+
+int main(int argc, const char **argv) {
+	signal(SIGSEGV, sig_handler);
+	signal(SIGTERM, sig_handler);
+	signal(SIGHUP, sig_handler);
+	signal(SIGINT, sig_handler);
+
+	const char *run = getenv("RUNTEST");
+	if (run == NULL){
+		run = "";
+	}
+
+	for (int i=1;i<argc;i++){
+		const char *arg = argv[i];
+		if (strcmp(arg, "-r") == 0 || strcmp(arg, "-run") == 0 || strcmp(arg, "--run") == 0){
+			if (i+1==argc){
+				fprintf(stderr, "argument '%s' requires a value\n", arg);
+				exit(-1);	
+			}
+			arg = argv[i+1];
+			i++;
+			run = arg;
+		} else {
+			fprintf(stderr, "unknown argument '%s'\n", arg);
+			exit(-1);
+		}
+	}
+
+	for (int i=0;i<sizeof(tests)/sizeof(test);i++){
+		test t = tests[i];
+
+		if (!(strlen(run) == 0 || strcmp(run, t.name) == 0)){
+			continue;
+		}
+		char label[50];
+		sprintf(label, "    Test %s ... ", t.name); 
+		fprintf(stdout, "%-25s", label);
+		fflush(stdout);
+		if (!t.test()){
+			printf("\x1b[31m[failed]\x1b[0m\n");
+		} else{
+			printf("\x1b[32m[ok]\x1b[0m\n");
+		}
+	}
+}
+
+//#endif
